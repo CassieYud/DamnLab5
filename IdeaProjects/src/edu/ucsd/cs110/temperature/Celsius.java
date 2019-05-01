2e0ca68b880bf3f57c4e4c8c1ca6f7274b3c8a3c
CassieYud <duy003@ucsd.edu>
2019-05-01 14:14:37 -0700

    first

---
+++ IdeaProjects/src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,29 @@
+package edu.ucsd.cs110.temperature;
+
+import java.awt.*;
+
+public class Celsius extends Temperature {
+    float temp = 0;
+    public Celsius(float t) {
+        super(t);
+        this.temp = t;
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        Temperature toReturn = new Celsius(this.temp);
+        return toReturn;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        float t = (float) (this.temp * 1.8 + 32);
+        Temperature toReturn = new Fahrenheit(t);
+        return toReturn;
+    }
+
+    public String toString() {
+        return ("" + this.temp + " C");
+
+    }
+}
